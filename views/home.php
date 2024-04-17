2531d55488e5adae0affa634e52ace6b764b63e8
Phuc Nguyen <nngiaphuc0101@gmail.com>
2024-04-17 23:37:42 +0700

    feat: add header and footer styles

--- views/home.php
+++ views/home.php
@@ -4,8 +4,17 @@
     require_once 'includes/header.php';
 ?>
 
-<div class="hero container-fluid text-center d-flex justify-content-center align-items-center">
-    <h1>Welcome to Hired CMUT!</h1>
+<div class="hero container-fluid text-center d-flex flex-column justify-content-center align-items-center">
+    <h1>Welcome to HiredCMUT!</h1>
+    <p>Find a job in an instant!</p>
+    <!-- button to explore now, scrolls down to new section -->
+    <a id="explore-btn" href="#explore">Explore Now</a>
+</div>
+
+<div id="explore" class="container my-5">
+    <h2 class="text-center">Popular Jobs</h2>
+    <div class="row">
+</div>
 </div>
 <?php
     require_once 'includes/footer.php';
