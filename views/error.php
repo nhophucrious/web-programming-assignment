eafc7299a2581026d80335f3bdb11080980fb4d3
Phuc Nguyen <nngiaphuc0101@gmail.com>
2024-04-22 13:29:11 +0700

    style: change error page style

--- views/error.php
+++ views/error.php
@@ -1,17 +1,26 @@
 <?php
     require_once 'includes/header.php';
 ?>
+<div class="container d-flex flex-column" style="min-height: 100vh;">
+    <div class="d-flex flex-column container my-3 p-3">
+        <h1 class="mb-5" style="font-weight: bold"><span style="color: black; background-color: #FFBF00; padding: 0 5px; border-radius: 10px;">404</span> not found! 😵</h1>
+        
+        <p>🤔 The page you are looking for does not exist. Please check the URL and try again.</p>
 
-<div class="container-fluid error-hero">
+    </div>
+
+    <!-- action buttons -->
+    <div class="container d-flex flex-row">
+        <div class="mr-3">
+            <a href="/web-programming-assignment/" class="hiredcmut-button">🏠 Go back to home</a>
+        </div>
+
+        <div>
+            <a href="/web-programming-assignment/jobs" class="hiredcmut-button">🔍 View job listings</a>
+        </div>
+    </div>
 </div>
 
-<div class="d-flex flex-column container my-3 p-3">
-    <h1 class="mb-2" style="font-weight: bold"><span style="color: black; background-color: #FFBF00; padding: 0 5px; border-radius: 10px;">404</span> not found!</h1>
-    
-    <p>🤔 The page you are looking for does not exist. Please check the URL and try again.</p>
-    <p>😉 By the way, we won't let your career be 404!</p>
-
-</div>
 
 <?php
     require_once 'includes/footer.php';
--- views/home.php
+++ views/home.php
@@ -6,7 +6,8 @@
 
 <div class="hero py-5 container-fluid text-center d-flex flex-column justify-content-center align-items-center">
     <div class="hero-content container py-5" style="width: 100% !important">
-        <h1 class="mb-5"><span style="color: black; background-color: #FFBF00; padding: 0 5px; border-radius: 10px;">Get employed</span> with HiredCMUT!</h1>        <p>Find a job in an instant!</p>
+        <h1 class="mb-5"><span style="color: black; background-color: #FFBF00; padding: 0 5px; border-radius: 10px;">Get employed</span> with HiredCMUT! 💪</h1>        
+        <p>Find a job in an instant!</p>
         <!-- button to explore now, scrolls down to new section -->
         <a id="explore-btn" href="#explore">Explore Now</a>
     </div>
