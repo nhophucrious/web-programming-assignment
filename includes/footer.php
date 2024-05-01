806dc7bd6fd764ead29577328101019f70e6d086
Phuc Nguyen <nngiaphuc0101@gmail.com>
2024-05-01 22:37:12 +0700

    fix: fix footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,24 +1,16 @@
+<style>
+.footer {
+    background-color: black !important;
+}
+</style>
 <footer class="footer bg-dark text-white mt-auto">
     <div class="container py-3">
         <div class="row">
             <div class="col-md-6">
                 <h2 class="h5">HiredCMUT Job Portal</h2>
-                <nav>
-                    <ul class="list-unstyled">
-                        <li><a href="index.php" class="text-white">Home</a></li>
-                        <li><a href="about.php" class="text-white">About</a></li>
-                        <li><a href="contact.php" class="text-white">Contact</a></li>
-                    </ul>
-                </nav>
+                
             </div>
-            <div class="col-md-6 text-md-right">
-                <small>&copy; <?php echo date('Y'); ?> HiredCMUT</small>
-                <div class="mt-2">
-                    <a href="#" class="text-white"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" class="text-white ml-2"><i class="fab fa-twitter"></i></a>
-                    <a href="#" class="text-white ml-2"><i class="fab fa-instagram"></i></a>
-                </div>
-            </div>
+            
         </div>
     </div>
 </footer>
